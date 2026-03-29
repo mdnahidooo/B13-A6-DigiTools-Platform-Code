7772a85025fdb499b9da74821e6bcbc49cc322f0
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-03-30 01:19:16 +0600

    Added Navbar perfectly with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
 
   return (
     <>
-      <h2 className='text-5xl bg-red-500 text-center'>Tailwind and DaisyUI setup checking.</h2>
-      {/* name of each tab group should be unique */}
-      <div className="tabs tabs-box">
-        <input type="radio" name="my_tabs_1" className="tab" aria-label="Tab 1" />
-        <input type="radio" name="my_tabs_1" className="tab" aria-label="Tab 2" defaultChecked />
-        <input type="radio" name="my_tabs_1" className="tab" aria-label="Tab 3" />
-      </div>
+      <Navbar></Navbar>
     </>
   )
 }
